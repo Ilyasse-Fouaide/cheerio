e228e2137ec7e3c05973de2dd0a361e0e95ed227
ilyasse-fouaide <ilyasse.fouaide@gmail.com>
2024-05-25 14:18:02 +0100

    basic setup for puppeteer

---
+++ puppeteer/index.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const newPage = await browser.newPage();
+
+	await newPage.goto('https://github.com/')
+})();
